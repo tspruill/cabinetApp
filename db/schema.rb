3ced9ee57cc19db35ba9bf00e3a359c62876303f
Tristen Spruill <Tspruil6@uncc.edu>
2018-11-15 11:56:34 -0500

    Association with User and Doc

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_091510) do
+ActiveRecord::Schema.define(version: 2018_11_15_164545) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
