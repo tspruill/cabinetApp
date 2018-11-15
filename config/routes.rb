cbc95b2c9dc6ac67b92f304f9d7da124d2ec2e0b
Tristen Spruill <Tspruil6@uncc.edu>
2018-11-15 12:10:19 -0500

    Added Authentication for user and changed root page for user who is authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root :to => redirect('/welcome/index')
+    
   resources :docs
+  authenticated :user do
+  root "docs#index", as: "auth_root"
+  end
+    
+  root :to => redirect('/welcome/index')
+  
 end
